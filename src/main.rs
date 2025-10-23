450e9ebbe80320f01f12867230a26427b3efff83
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-23 13:24:06 +0530

    Splited the guessing game into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,5 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io::{self, Stdin};
+mod guessing_game;
 
 fn main() {
-    let stdin: Stdin = io::stdin();
-    let mut tries: u32 = 3;
-    let secret_number: u32 = rand::rng().random_range(1..=10);
-
-    loop {
-        let mut guess: String = String::new();
-
-        println!("Guess a number: ");
-
-        stdin.read_line(&mut guess).expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Enter a valid number\n");
-                continue;
-            }
-        };
-
-        match guess.cmp(&secret_number) {
-            Ordering::Equal => {
-                println!("You got it correct!");
-                break;
-            }
-            Ordering::Greater => println!("Too big!\n"),
-            Ordering::Less => println!("Too small!\n"),
-        };
-
-        tries -= 1;
-
-        println!("You have {tries} tries left!");
-
-        if tries == 0 {
-            println!("You lost!");
-            println!("The secret number was: {secret_number}");
-            break;
-        }
-    }
+    guessing_game::run();
 }
