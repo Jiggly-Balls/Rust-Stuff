c1714842964e06cc360bb63ae524ba5d42250a72
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-23 15:47:34 +0530

    Added board display function

--- src/learning/proj_bad_x_o.rs
+++ src/learning/proj_bad_x_o.rs
@@ -1,2 +1,20 @@
 #[allow(dead_code)]
-pub fn run() {}
+pub fn run() {
+    let board: [[i8; 3]; 3] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    display_board(&board);
+}
+
+fn display_board(board_arr: &[[i8; 3]; 3]) -> () {
+    for row in board_arr.iter() {
+        for col in row.iter() {
+            if *col == 0 {
+                print!("- ");
+            } else if *col == 1 {
+                print!("X ");
+            } else {
+                print!("O ");
+            }
+        }
+        print!("\n");
+    }
+}
