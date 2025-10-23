98e4983b4b2a2798c5079d7e1547eff2a553cb4c
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-23 16:45:09 +0530

    Added input handling

--- src/learning/proj_bad_x_o.rs
+++ src/learning/proj_bad_x_o.rs
@@ -1,7 +1,10 @@
+use std::io;
+
 #[allow(dead_code)]
-pub fn run() {
+pub fn run() -> () {
     let board: [[i8; 3]; 3] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     display_board(&board);
+    handle_board_input(&board);
 }
 
 fn display_board(board_arr: &[[i8; 3]; 3]) -> () {
@@ -16,5 +19,39 @@ fn display_board(board_arr: &[[i8; 3]; 3]) -> () {
             }
         }
         print!("\n");
+    }
+}
+
+fn handle_board_input(board_arr: &[[i8; 3]; 3]) -> () {
+    let row_input: i8 = get_input("Enter the row: ");
+    let col_input: i8 = get_input("Enter the column: ");
+
+    println!("{row_input} {col_input}");
+}
+
+fn get_input(prompt: &str) -> i8 {
+    loop {
+        let mut input_data: String = String::new();
+
+        println!("{prompt}");
+
+        io::stdin()
+            .read_line(&mut input_data)
+            .expect("Couldn't read the input");
+
+        let input_data: i8 = match input_data.trim().parse() {
+            Ok(input_data) => input_data,
+            Err(_) => {
+                println!("Enter a number from 1-3!");
+                continue;
+            }
+        };
+
+        if input_data < 1 || input_data > 3 {
+            println!("Enter a number from 1-3!");
+            continue;
+        }
+
+        return input_data;
     }
 }
