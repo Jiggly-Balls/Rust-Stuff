c659b6e9a73d42066682e25a0cc4617ad90033be
Jiggly Balls <jigglyballs9000@gmail.com>
2025-10-24 19:10:16 +0530

    Typo fix

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ mod learning;
 mod projects;
 
 fn main() {
-    // learning::proj_guessing_game::run();
     // learning::rust_arrays::run();
     // learning::rust_tuples::run();
+    // projects::proj_guessing_game::run();
 
     projects::proj_bad_x_o::run();
 }
